5bda8fe2233adb0c291644e836bcb90f5e87d293
josifpeev <josif.peev@gmail.com>
2023-02-04 16:12:02 +0200

    update files

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,17 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+resource "aws_security_group" "ports" {
+  name        = "ports-security-group"
+  description = "Allow inbound traffic example"
+}
+
 resource "aws_instance" "plesk01" {
   ami           = "ami-0d385ef28de9ef4ea"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.ports.id]
    tags = {
     Name = "plesk01.final-project.com"
   }
-    security_groups = [
-    aws_security_group.ports
-  ]
+  
 }
