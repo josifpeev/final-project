bf1d2d053eb0794b123c88dbdd05661585901c74
josifpeev <josif.peev@gmail.com>
2023-02-04 16:13:18 +0200

    u1

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,6 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_security_group" "ports" {
-  name        = "ports-security-group"
-  description = "Allow inbound traffic example"
-}
-
 resource "aws_instance" "plesk01" {
   ami           = "ami-0d385ef28de9ef4ea"
   instance_type = "t2.micro"
