baba3208681c7b28bf308d0b0c49e1f0c45113c6
josifpeev <josif.peev@gmail.com>
2023-02-04 18:18:00 +0200

    update2

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,13 +5,15 @@ provider "aws" {
 
 # Our Ples servers
 
+######################## Plesk01 ############################
+
 resource "aws_eip" "plesk01" {
   vpc         = true
   instance    = aws_instance.plesk01.id
 }
 
 resource "aws_instance" "plesk01" {
-  ami           = "var.ami"
+  ami           = var.ami
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.ports.id]
   key_name = "plesk_key_pair"
@@ -20,3 +22,5 @@ resource "aws_instance" "plesk01" {
   }
   
 }
+
+################ End configuration for Plesk01 #############
