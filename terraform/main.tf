0b59ee105f0847d220b40faceda9320255298e9b
josifpeev <josif.peev@gmail.com>
2023-02-04 17:41:15 +0200

    add keypair

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,16 @@
+# AWS provider
 provider "aws" {
   region = "eu-west-1"
 }
+
+# Our Ples servers
 
 resource "aws_instance" "plesk01" {
   ami           = "ami-0d385ef28de9ef4ea"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.ports.id]
-  public_ipv4_pool = "amazon"
+  key_name = "plesk_key_pair"
+  associate_public_ip_address = true
    tags = {
     Name = "plesk01.final-project.com"
   }
