e316e20c7c5deaa29f8ea3ed512862e60d122066
josifpeev <josif.peev@gmail.com>
2023-02-04 13:52:16 +0200

    update main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_instance" "plesk01.final-project.com" {
+resource "aws_instance" "plesk01" {
   ami           = "ami-0d385ef28de9ef4ea"
   instance_type = "t2.micro"
 }
