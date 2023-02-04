579279c7a63d401160a60040b08986919b173c34
josifpeev <josif.peev@gmail.com>
2023-02-04 17:26:28 +0200

    test

--- terraform/elasticip.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_eip" "plesk01" {
-  vpc = true
-  associate_with_private_ip = aws_instance.plesk01.private_ip
-}
--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "plesk01" {
   ami           = "ami-0d385ef28de9ef4ea"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.ports.id]
+  public_ipv4_pool = "amazon"
    tags = {
     Name = "plesk01.final-project.com"
   }
