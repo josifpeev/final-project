0ad21f4f047f4167394cadd78656df6831cf5f03
josifpeev <josif.peev@gmail.com>
2023-02-04 14:21:37 +0200

    add open ports

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,4 +5,7 @@ provider "aws" {
 resource "aws_instance" "plesk01" {
   ami           = "ami-0d385ef28de9ef4ea"
   instance_type = "t2.micro"
+   tags = {
+    Name = "plesk01.final-project.com"
+  }
 }
