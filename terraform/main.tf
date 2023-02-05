71c98a409db7aa85267cba037767fc3f997c8a9f
josifpeev <josif.peev@gmail.com>
2023-02-05 13:20:27 +0200

    destroy one instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,23 +24,3 @@ resource "aws_instance" "plesk01" {
 }
 
 ################ End configuration for Plesk01 #############
-
-######################## Plesk02 ############################
-
-#resource "aws_eip" "plesk01" {
-#  vpc         = true
-#  instance    = aws_instance.plesk02.id
-#}
-
-resource "aws_instance" "plesk02" {
-  ami           = var.ami
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.ports.id]
-  key_name = "plesk_key_pair"
-    tags = {
-    Name = "plesk02.final-project.com"
-  }
-  
-}
-
-################ End configuration for Plesk02 #############
