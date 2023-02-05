19cd46e7a4cd8cbd8e61dc823149db9a93fcbc95
josifpeev <josif.peev@gmail.com>
2023-02-06 01:58:48 +0200

    u2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "ami" {
   type        = string
   description = "The ID of the Amazon Machine Image (AMI) to use for the EC2 instance"
-  default     = "ami-0622422f35c2c10da"
+  default     = "ami-0a65754762d58cb30"
 }
