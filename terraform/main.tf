2126679d7e63e59431840279ca4ee9879ef175d6
josifpeev <josif.peev@gmail.com>
2023-02-09 23:39:33 +0200

    update file

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,9 +47,9 @@ EOF
        host        = self.public_ip
      }
   }
-  
+
   provisioner "local-exec" {
-     command = "ansible-playbook -i ${aws_instance.plesk01.public_ip}, --private-key ${local.private_key_path} ansible/all.yaml"
+     command = "ansible-playbook -i ${aws_instance.plesk01.public_ip}, --private-key ${local.private_key_path} ../ansible/all.yaml"
    }
 
 
